ad8201501b80a555345e69d067b74fc158ae3f24
KimSuro <kimsuro5773@gmail.com>
2026-04-22 00:09:50 +0900

    feat: 라우터 및 전역 프로바이더 설정, 페이지 구조 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import { Button } from "./components/ui/button";
+import RootRoute from "./root-route";
 
-function App() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl">초기 세팅</h1>
-      <Button>버튼 테스트</Button>
-      <Button variant={"destructive"}>버튼 테스트</Button>
-    </div>
-  );
+export default function App() {
+  return <RootRoute />;
 }
-
-export default App;
