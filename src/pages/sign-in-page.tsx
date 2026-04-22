4b0e724a7ec6263921d56483e18c23a53a2267bc
KimSuro <kimsuro5773@gmail.com>
2026-04-22 15:40:03 +0900

    feat: 로그인 기능 구현
    - signInWithPassword API 연동 (src/api/auth.ts)
    - useSignInWithPassword mutation hook 추가
    - 로그인 폼 UI 및 제출 로직 구현

--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -1,3 +1,55 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-password";
+import { useState } from "react";
+import { Link } from "react-router";
+
 export default function SignInPage() {
-  return <div>SignInPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { mutate: signInWithPassword, isPending } = useSignInWithPassword();
+
+  const handleSignInWithPasswordClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+
+    signInWithPassword({ email, password });
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="text-center text-xl font-bold">로그인</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          className="py-6"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="이메일"
+        />
+        <Input
+          className="py-6"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="패스워드"
+        />
+      </div>
+      <div>
+        <Button
+          disabled={isPending}
+          className="w-full"
+          onClick={handleSignInWithPasswordClick}
+        >
+          로그인
+        </Button>
+      </div>
+      <div>
+        <Link className="text-muted-foreground hover:underline" to={"/sign-up"}>
+          계정이 없으시다면? 회원가입
+        </Link>
+      </div>
+    </div>
+  );
 }
